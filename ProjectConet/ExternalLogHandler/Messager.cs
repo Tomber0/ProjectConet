954916e5ad502fd225a9c4730267019b79a16a74
Aleksey.Zaitsau <llzaicev@yandex.ru>
2022-11-19 14:33:12 +0300

    added exceptions handler

---
+++ ProjectConet/ExternalLogHandler/Messager.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectConet.ExternalLogHandler
+{
+    internal abstract class Messager
+    {
+        public abstract string WriteMessage(string message);
+    }
+}
--- ProjectConet/VideoDownload/VideoDownloaderFromYoutube.cs
+++ ProjectConet/VideoDownload/VideoDownloaderFromYoutube.cs
@@ -8,9 +8,24 @@ namespace ProjectConet.VideoDownload
         public async override void Download(string url, string fileDirUrl, string newFileName)
         {
             var youTube = YouTube.Default;
-            var video = await youTube.GetVideoAsync(url); 
+            object videoTask = null;
+            try
+            {
+                videoTask = await youTube.GetVideoAsync(url); 
+            }
+            catch (Exception ex)
+            {
+                Logging.Logger.Instance.Error(ex.Message);
+            }
             var videoPath = $"{fileDirUrl}\\" + $"{newFileName}" + ".mp4";
-            await File.WriteAllBytesAsync($"{videoPath}", video.GetBytes());
+            try
+            {
+                await File.WriteAllBytesAsync($"{videoPath}", ((YouTubeVideo)videoTask).GetBytes());
+            }
+            catch (Exception ex)
+            {
+                Logging.Logger.Instance.Error(ex.Message);
+            }
         }
 
         public override void Convert(string path)
